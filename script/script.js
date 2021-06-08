76bac0d772914cbb384641bf56d6caff82555316
FrankieRG <garciaf90@gmail.com>
2021-06-08 01:18:38 -0700

    hw

---
+++ script/script.js
@@ -0,0 +1,71 @@
+let quotes = [
+  `I live my life a quarter mile at a time`,
+  `I said a ten-second car, not a ten-minute car`,
+  `You can have any brew you want... as long as it's a Corona.`,
+  `You almost had me? You never had me - you never had your car!`,
+  `I don't have friends. I have family.`,
+  `It don't matter if you win by an inch or a mile. Winning's winning.`
+];
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Random quote of the day generator
+  const randomQuote = function() {
+    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+  };
+  randomQuote();
+
+  // Do all of your work inside the document.addEventListener
+
+  // Part 1
+const mainTitle = document.querySelector('#main-title')
+mainTitle.innerHTML = '<strong> The names Dom. when youre here, youre family </strong>'
+  // Part 2
+const body = document.querySelector('body')
+body.style.backgroundColor = 'red'
+
+  // Part 3
+  const favoriteThings = document.querySelector('#favorite-things');
+  const sideBar = document.querySelector('.side-bar li');
+  sideBar.parentNode.removeChild(staring people down);
+
+  // Part 4
+const quoteTitle = document.querySelector('#quote-title')
+style.fontSize = '2rem';
+
+const domAdventures = document.querySelector('#dom-adventures')
+style.fontSize = '2rem';
+
+  // Part 5
+const pastRaces = document.querySelector('#past-races');
+const sideBar = document.querySelector('.side-bar li');
+sideBar.parentNode.removeChild(Chicago);
+
+  // Part 6
+function sideBar() {
+  li.appendChild(document.createTextNode("six"));
+  li.setAttribute("id", "element6");
+  ul.appendChild(New York);
+}
+
+  // Part 7
+const blogPost = document.createElement("h2")
+document.createTextNode(New York)
+const blogPost = document.createElement("p")
+document.createTextNode("they taught me how to say deadass properly")
+blogPost.appendChild(text);
+const new = document.getElementById("new");
+element.appendChild(tag);
+
+  // Part 8
+const quote = document.querySelector('#quote-title')
+forEach(randomQuote => {item.addEventListener('click', event => { // handle click 
+  })
+})
+addEventListener("click", )
+
+  // Part 9
+
+
+
+
+});
